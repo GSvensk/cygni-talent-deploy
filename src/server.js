d587f651ede87d6e1a6e5cea3855f5c57770952c
Gustav Svensk <gustav.svensk95@gmail.com>
2021-04-14 16:52:26 +0200

    prettier

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ function createServer(config = {}) {
 
   const server = http.createServer((req, res) => {
     console.log(`Received request ${req.method.toUpperCase()} ${req.url}`);
-    let a = 5
+    let a = 5;
     res.writeHead(200, "OK");
     res.write(Buffer.from(`Hello Cygni! Build number ${buildNumber}\n`));
     res.end();
